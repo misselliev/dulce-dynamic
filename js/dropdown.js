9abc5306bef758f277f9785caba41e8e3a493c51
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-29 17:48:33 -0700

    basic html for form

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,5 +1,9 @@
+// Navbar dropdown
+
 document.getElementById('shop').addEventListener('click', openMenu);
 
 function openMenu() {
   document.getElementById('dropdown').classList.toggle('active');
 }
+
+// Form
